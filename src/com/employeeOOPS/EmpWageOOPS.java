ad1e0a21ac3d45a7265dee1cc9aaac8e2737169f
SurabhiSanjan <surabhisanjan@gmail.com>
2022-11-03 14:52:36 +0530

    Modification: check Employee is Present or Absent

--- src/com/employeeOOPS/EmpWageOOPS.java
+++ src/com/employeeOOPS/EmpWageOOPS.java
@@ -1,4 +1,15 @@
 package com.employeeOOPS;
-
+import java.util.Random;
 public class EmpWageOOPS {
+    public static void main(String args[]) {
+
+        System.out.println("Welcome to Employee Wage Program");
+        Random r = new Random();
+        int present = r.nextInt(2);
+        if (present == 0) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
+    }
 }
