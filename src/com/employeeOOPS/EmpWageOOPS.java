2e8ccb47621d2766632608926e221bb7fec53fb0
SurabhiSanjan <surabhisanjan@gmail.com>
2022-11-05 19:20:26 +0530

    Modification: applying OOPS concept for multiple companies

--- src/com/employeeOOPS/EmpWageOOPS.java
+++ src/com/employeeOOPS/EmpWageOOPS.java
@@ -2,45 +2,50 @@ package com.employeeOOPS;
 import java.util.*;
 
 public class EmpWageOOPS {
-    static int wage = 20;
-    static int hour;
+
     public static void main(String args[]) {
         System.out.println("Welcome to Employee Wage Program");
-        condition();
+        condition("TCS", 20, 100, 18);
+        condition("HCL", 22, 80, 20);
     }
-    public static void condition() {
-        int count = 0; //to keep a count of number of days worked
-        while (hour != 100) {
+    public static void condition(String Name, int wage, int hour, int days) {
+        System.out.println("Company Name " + Name);
+        System.out.println("Wage per hour: " + wage);
+        System.out.println("Maximum working days: " + days);
+        System.out.println("Satisfactory working hours: " + hour);
+        int count = 0;//to keep a count of number of days worked
+        int i=0;
+        while(i!= hour) {
             Random r = new Random();
             int present = r.nextInt(3);
             switch (present) {
                 case 0: {
-                    System.out.println("Employee is present: Full time");
-                    hour = hour + 8;
+                   // System.out.println("Employee is present: Full time");
+                    i = i + 8;
                     count++;
                 }
                 break;
                 case 1: {
-                    System.out.println("Employee is present: Part time");
-                    hour = hour + 4;
+                    //System.out.println("Employee is present: Part time");
+                    i = i + 4;
                     count++;
                 }
                 break;
                 default:
-                    System.out.println("Employee is absent");
-                    hour = hour + 0;
+                    //System.out.println("Employee is absent");
+                    i = i + 0;
                     count++;
             }
 
         }
-        if (count == 20) {
+        if (count == days) {
             System.out.println("Employee Satisfied total working hours in a month i.e; 100 hours in 20 working days");
         } else {
             System.out.println("Employee Not Satisfied total working hours in a month ");
         }
         int dailyWage = wage * hour;
         System.out.println("Daily Employee wage: " + dailyWage);
-        System.out.println("Monthly  wage: " + dailyWage*20);
+        System.out.println("Monthly  wage: " + dailyWage*days);
     }
 }
 
