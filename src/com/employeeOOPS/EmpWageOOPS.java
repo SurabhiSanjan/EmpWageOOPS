98b51741b875f19dad651e057bb74b41e3497422
SurabhiSanjan <surabhisanjan@gmail.com>
2022-11-05 18:49:40 +0530

    Refactor: applying OOPS concept

--- src/com/employeeOOPS/EmpWageOOPS.java
+++ src/com/employeeOOPS/EmpWageOOPS.java
@@ -1,47 +1,50 @@
 package com.employeeOOPS;
 import java.util.*;
+
 public class EmpWageOOPS {
+    static int wage = 20;
+    static int hour;
     public static void main(String args[]) {
-        int wage = 20;
-        int hour = 0;
         System.out.println("Welcome to Employee Wage Program");
-        int count = 0;
-        while(hour==100){
+        condition();
+    }
+    public static void condition() {
+        int count = 0; //to keep a count of number of days worked
+        while (hour != 100) {
             Random r = new Random();
             int present = r.nextInt(3);
             switch (present) {
                 case 0: {
                     System.out.println("Employee is present: Full time");
-                    hour = 8;
-                    // System.out.println("Employee is present: Full time");
                     hour = hour + 8;
                     count++;
                 }
                 break;
                 case 1: {
                     System.out.println("Employee is present: Part time");
-                    hour = 4;
-                    // System.out.println("Employee is present: Part time");
-                    hour = hour+ 4;
+                    hour = hour + 4;
                     count++;
                 }
                 break;
                 default:
                     System.out.println("Employee is absent");
-                    hour = 0;
                     hour = hour + 0;
                     count++;
             }
 
         }
-        if(count==20){
-        System.out.println("Employee Satisfied total working hours in a month i.e; 100 hours in 20 working days");
-    }
-        else {
-        System.out.println("Employee Not Satisfied total working hours in a month ");
-    }
-    int dailyWage = wage * hour;
-    System.out.println("Daily Employee wage: " + dailyWage);
-    System.out.println("Monthly  wage: " + dailyWage*20);
+        if (count == 20) {
+            System.out.println("Employee Satisfied total working hours in a month i.e; 100 hours in 20 working days");
+        } else {
+            System.out.println("Employee Not Satisfied total working hours in a month ");
+        }
+        int dailyWage = wage * hour;
+        System.out.println("Daily Employee wage: " + dailyWage);
+        System.out.println("Monthly  wage: " + dailyWage*20);
     }
 }
+
+
+
+
+
